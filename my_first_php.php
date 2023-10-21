55c7f649ad00594b8e8f6f7bf3bd4db6635e6966
Kosta Kazakov <kostakazakoff@gmail.com>
2023-10-21 18:43:14 +0300

    create first class in PHP

--- my_first_php.php
+++ my_first_php.php
@@ -1,2 +1,19 @@
-$x = 5;
-echo $x;
+class MyClass
+{
+    public $firstname;
+    public $familyname;
+
+    public function __construct($firstname = null, $familyname = null)
+    {
+        $this->firstname = $firstname;
+        $this->familyname = $familyname;
+    }
+
+    public function great()
+    {
+        return "Hello, " . $this->firstname . " " . $this->family;
+    }
+}
+
+$me = new MyClass("Kosta", "Kazakov");
+echo $me->great();
