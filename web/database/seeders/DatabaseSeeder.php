d9bd33f084cbcb7c8407fe4b93348d7b255b8780
Kosta Kazakov <kostakazakoff@gmail.com>
2023-10-23 00:25:29 +0300

    RealEstateFactory created

--- web/database/seeders/DatabaseSeeder.php
+++ web/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\RealEstates;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+
+        RealEstates::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
