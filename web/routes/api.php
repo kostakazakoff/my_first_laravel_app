ca1443b5b3cba35e7212138477805f610d9e2b8b
Kosta Kazakov <kostakazakoff@gmail.com>
2023-10-22 22:55:27 +0300

    RealEstates model and table created

--- web/routes/api.php
+++ web/routes/api.php
@@ -14,6 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
+API Routes:
+'/' => 'index',
+'/real_estates' => 'All real_estates',
+'/real_estates/{id}' => 'real_estate with id',
+'/real_estates/create' => 'Create real estate',
+'/real_estates/put/{id}' => 'Update real estate with id',
+'/real_estates/delete/{id}' => 'Delete real estate with id',
+*/
+
+Route::get('/real_estates', function() {
+    return response()->json(['real_estates']);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
