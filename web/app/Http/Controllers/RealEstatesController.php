ce1b977981995dd89515bb5367aaa0aa931439a7
Kosta Kazakov <kostakazakoff@gmail.com>
2023-10-25 15:36:23 +0300

    refactoring

--- web/app/Http/Controllers/RealEstatesController.php
+++ web/app/Http/Controllers/RealEstatesController.php
@@ -2,48 +2,55 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreEstateRequest;
 use App\Models\RealEstate;
+use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use App\Http\Requests\StoreEstateRequest;
 
 
 class RealEstatesController extends Controller
 {
-    public function list(): ?JsonResponse {
+
+    public function list(): ?JsonResponse
+    {
         $list = RealEstate::all();
 
-        return response()->json(['Valid'=>$list]);
+        return response()->json(['Valid' => $list]);
     }
 
-    public function show(string $id): JsonResponse {        
+    public function show(string $id): JsonResponse
+    {
         $realEstate = RealEstate::findOrFail($id);
 
-        return response()->json(['Valid'=>$realEstate]);
+        return response()->json(['Valid' => $realEstate]);
     }
 
-    public function store(StoreEstateRequest $request): JsonResponse {
+    public function store(StoreEstateRequest $request): JsonResponse
+    {
         $data = $request->json()->all();
-        
+
         $realEstate = RealEstate::create($data);
-        
-        return response()->json(['Valid'=>$realEstate]);
+
+        return response()->json(['Valid' => $realEstate]);
     }
 
-    public function update(StoreEstateRequest $request, string $id): JsonResponse {
+    public function update(StoreEstateRequest $request, string $id): JsonResponse
+    {
         $realEstate = RealEstate::findOrFail($id);
 
         $data = $request->json()->all();
 
         $realEstate->update($data);
 
-        return response()->json(['Valid'=>$realEstate]);
+        return response()->json(['Valid' => $realEstate]);
     }
 
-    public function delete(string $id): JsonResponse {
+    public function delete(string $id): JsonResponse
+    {
         $realEstate = RealEstate::findOrFail($id);
 
         $realEstate->delete();
 
-        return response()->json(['message' => $realEstate['title'].' deleted']);
+        return response()->json(['message' => $realEstate['title'] . ' deleted']);
     }
 }
